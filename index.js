4f74046329cb3ceb7490d943fe10d8ff08fa2f3e
iabbakr <abubakaribrahim603@gmail.com>
2022-12-30 14:47:43 +0100

    added js

---
+++ index.js
@@ -0,0 +1,19 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let disp = document.getElementById("#display");
+let conv = document.getElementById("converter");
+
+let convEl = 0;
+
+
+conv.addEventListener("click", function(){
+    let hi = conv + 1;
+    disp.textContent += hi;
+});
+
+
+
