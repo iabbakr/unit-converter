6bb9003881352e0e2ec61d0167c28e70131512d5
iabbakr <abubakaribrahim603@gmail.com>
2023-01-01 15:53:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 1 kilogram = 2.204 pound
 */
 
-let disp = [];
-let displayEl = document.getElementById("display");
-let convertEl = document.getElementById("converter");
-let lenEl = document.getElementById("length");
-let volEl = document.getElementById("volume");
-let massEl = document.getElementById("mass");
+const disp = [];
+const displayEl = document.getElementById("display");
+const convertEl = document.getElementById("converter");
+const lenEl = document.getElementById("length");
+const volEl = document.getElementById("volume");
+const massEl = document.getElementById("mass");
 
 const feet = 3.281;
 const meter = 0.304;
@@ -24,50 +24,34 @@ const kilo = 0.453;
 
 convertEl.addEventListener("click", function(){
     disp.push(displayEl.value);
-
-    
     len();
     vol();
     mas();
 });
-
 function len(){
-    
-
-    let a = parseInt(disp) * feet;
-    let b = parseInt(disp) * meter;
-
-    let mF = `
+    const a = parseInt(disp) * feet;
+    const b = parseInt(disp) * meter;
+    const mF = `
         <p>
            ${disp} meters = ${a.toFixed(2)} feets | ${disp} feets = ${b} meters
         <p>`;
-    
     lenEl.innerHTML += mF;
 };
-
 function vol(){
-
-    let c = parseInt(disp) * gallon;
-    let d = parseInt(disp) * liter;
-
-    let lG = `
+    const c = parseInt(disp) * gallon;
+    const d = parseInt(disp) * liter;
+    const lG = `
         <p>
            ${disp} liters = ${c.toFixed(2)} gallons | ${disp} gallons = ${d} liters
         <p>`;
-    
     volEl.innerHTML += lG;
 };
-
-
 function mas(){
-
-    let e = parseInt(disp) * pound;
-    let f = parseInt(disp) * kilo;
-
-    let kP = `
+    const e = parseInt(disp) * pound;
+    const f = parseInt(disp) * kilo;
+    const kP = `
         <p>
            ${disp} kilos = ${e.toFixed(2)} pounds | ${disp} pounds = ${f} kilos
         <p>`;
-    
     massEl.innerHTML += kP;
 };
