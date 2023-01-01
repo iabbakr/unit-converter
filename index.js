4a0d56e45259b1548a764a6cb7702704178ee869
iabbakr <abubakaribrahim603@gmail.com>
2023-01-01 15:36:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,66 @@
 let disp = [];
 let displayEl = document.getElementById("display");
 let convertEl = document.getElementById("converter");
+let lenEl = document.getElementById("length");
+let volEl = document.getElementById("volume");
 let massEl = document.getElementById("mass");
 
-
-let pounds = 2.204;
-let kilos = 0.453;
+const feet = 3.281;
+const meter = 0.304;
+const gallon = 0.264;
+const liter = 3.785;
+const pound = 2.204;
+const kilo = 0.453;
 
 
 
 
 
 convertEl.addEventListener("click", function(){
+    len();
+    vol();
     mas();
 });
+
+function len(){
+    disp.push(displayEl.value);
+
+    let a = parseInt(disp) * feet;
+    let b = parseInt(disp) * meter;
+
+    let mF = `
+        <p>
+           ${disp} meters = ${a.toFixed(2)} feets | ${disp} feets = ${b} meters
+        <p>`;
+    
+    lenEl.innerHTML += mF;
+};
+
+function len(){
+    disp.push(displayEl.value);
+
+    let c = parseInt(disp) * gallon;
+    let d = parseInt(disp) * liter;
+
+    let lG = `
+        <p>
+           ${disp} liters = ${c.toFixed(2)} gallons | ${disp} gallons = ${d} liters
+        <p>`;
+    
+    volEl.innerHTML += lG;
+};
 
 
 function mas(){
     disp.push(displayEl.value);
 
-    let s = parseInt(disp) * pounds;
-    let l = parseInt(disp) * kilos;
+    let e = parseInt(disp) * pound;
+    let f = parseInt(disp) * kilo;
 
-    let hi = `
+    let kP = `
         <p>
-           ${disp} kilos = ${s.toFixed(2)} gallons | ${disp} pounds = ${l} kilos
+           ${disp} kilos = ${e.toFixed(2)} pounds | ${disp} pounds = ${f} kilos
         <p>`;
     
-    massEl.innerHTML += hi;
+    massEl.innerHTML += kP;
 };
